49be3983b3c6802fc67ad4f5b307cbf34aaf4494
manolotux <manolotux@hotmail.com>
2024-09-17 16:00:21 -0600

    Implematacion de security identity on WEB

---
+++ Sales/Sales.WEB/Auth/AuthProviderTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Sales.WEB.Auth
+{
+	public class AuthProviderTest : AuthenticationStateProvider
+	{
+		public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+			var anonimous = new ClaimsIdentity();
+			return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+		}
+	}
+}
--- Sales/Sales.WEB/Program.cs
+++ Sales/Sales.WEB/Program.cs
@@ -1,16 +1,22 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.WEB;
+using Sales.WEB.Auth;
 using Sales.WEB.Repositorios;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7092") });
+builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7092") });
 builder.Services.AddScoped<IRepositorio, Repositorio>();
 
 builder.Services.AddSweetAlert2();
+
+//Autorizacion
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthProviderTest>();
 
 await builder.Build().RunAsync();
